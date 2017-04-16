4059ecfcfb69a1364506f95f9b74efeb5f335c1a
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-04-16 14:30:25 +0200

    publicScenarios small form improvements

--- src/app/public-scenarios/public-scenarios.component.ts
+++ src/app/public-scenarios/public-scenarios.component.ts
@@ -10,23 +10,25 @@ import { PUBLIC_SCENARIOS } from '../mocks';
     templateUrl: './public-scenarios.component.html'
 })
 export class PublicScenariosComponent implements OnInit {
-   
-    pdfForm : FormGroup; 
+
+    pdfForm: FormGroup;
     formMessages = PDF_FORM_MSG;
     formHeader = 'Dodaj własny scenariusz';
     fileLabel = 'Plik PDF:';
     submitText = 'Dodaj';
+    emailPattern = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/;
 
     fileBase64 = '';
     scenarioStates: Array<string> = SCENARIO_STATES;
     scenarios: Array<PublicScenario> = PUBLIC_SCENARIOS;
 
-    constructor(formBuilder:FormBuilder) {
+    constructor(formBuilder: FormBuilder) {
         this.pdfForm = formBuilder.group({
-            title: ["",Validators.compose([Validators.required, Validators.minLength(1)])],
-            authorEmail: ["",Validators.compose([Validators.required,Validators.pattern("^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$")])],
-            state : ["", Validators.required],
-            description: ["",Validators.compose([Validators.required, Validators.minLength(10),Validators.maxLength(100)])],
+            title: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+            authorEmail: ['', Validators.compose([Validators.required,
+                              Validators.pattern(this.emailPattern)])],
+            state : ['', Validators.required],
+            description: ['', Validators.compose([Validators.required, Validators.minLength(10),Validators.maxLength(100)])],
         });
     }
 
@@ -37,26 +39,26 @@ export class PublicScenariosComponent implements OnInit {
         return !this.pdfForm.controls[controlName].valid && this.pdfForm.controls[controlName].dirty;
     }
 
-    fileChangeEvent(fileInput: any, context:any){
+    fileChangeEvent(fileInput: any, context: any) {
         if (fileInput.target.files && fileInput.target.files[0]) {
             const reader = new FileReader();
 
-            reader.onload = function (e : any) {
+            reader.onload = function (e: any) {
                 context.fileBase64 = e.target.result;
-            }
+            };
             reader.readAsDataURL(fileInput.target.files[0]);
         }
     }
 
-    submitPDF(submitted) {
+    submitPDF(submitted): void  {
         console.log(submitted);
     }
 
-    showAlert(): void{
+    showAlert(): void {
         alert('Na podany adres email wysłaliśmy ważne informacje. Dziękujemy za pomoc w rozwoju serwisu.');
     }
 
-    deleteScenario() :void{
+    deleteScenario(): void {
         confirm('Czy jesteś pewien?');
     }
 
