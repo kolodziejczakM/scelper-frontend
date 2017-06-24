663abdfb9a00a470b78aea03132a26310f891955
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-06-25 00:18:41 +0200

    footer: statute link hardcoded, publicScenarios: table design improved

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { environment } from '../../environments/environment';
+
 @Component({
     selector: 'sce-footer',
     templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 
-    private statuteLink = `${environment.serverRoot}/documents/Scelper-regulamin.pdf`;
+    private statuteLink = `https://scelper.com/documents/Scelper-regulamin.pdf`;
 
     public genesisTexts = new Map ([
         ['head', 'Stworzono z'],
         ['tail', 'do scenariopisarstwa i darmowego oprogramowania.']
     ]);
 
-    public links = [
-        { label: 'Regulamin i polityka prywatności', href: this.statuteLink },
-        { label: 'Twórcy', href: '/authors' }
-    ];
+    public links = {
+        statute: { label: 'Regulamin i polityka prywatności', href: this.statuteLink },
+        authors: { label: 'Twórcy', href: '/authors' }
+    };
 
 }
