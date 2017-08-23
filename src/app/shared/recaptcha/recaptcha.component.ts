ca03b8eaeff7c049adc8182b1c448a0c59bee06d
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-08-23 15:46:40 +0200

    Recaptcha fix

--- src/app/shared/recaptcha/recaptcha.component.ts
+++ src/app/shared/recaptcha/recaptcha.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit, Input, ApplicationRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ApplicationRef, ElementRef, ViewChild } from '@angular/core';
 import { WindowService } from '../../shared/window.service';
 
 @Component({
     selector: 'sce-recaptcha',
     templateUrl: './recaptcha.component.html'
 })
-export class RecaptchaComponent implements OnInit {
+export class RecaptchaComponent implements OnInit, AfterViewInit {
+
+    @ViewChild('wrapperNode')
+    public wrapperNode: ElementRef;
 
     @Input('siteKey')
     public siteKey: string;
+
 
     public approved = false;
 
@@ -20,6 +24,10 @@ export class RecaptchaComponent implements OnInit {
     ngOnInit() {
         this.registerCallback();
         this.registerExpiredCallback();
+    }
+
+    ngAfterViewInit() {
+        this.approved = (this.wrapperNode.nativeElement.height) ? false : true;
     }
 
     public isApproved(): boolean {
