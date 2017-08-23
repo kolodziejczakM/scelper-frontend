437f8578cf245c7ccb0bb3d1d7d282393ba43989
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-08-23 16:08:03 +0200

    simplest ver of recaptcha

--- src/app/shared/recaptcha/recaptcha.component.ts
+++ src/app/shared/recaptcha/recaptcha.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit, AfterViewInit, Input, ApplicationRef, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ApplicationRef } from '@angular/core';
 import { WindowService } from '../../shared/window.service';
 
 @Component({
     selector: 'sce-recaptcha',
     templateUrl: './recaptcha.component.html'
 })
-export class RecaptchaComponent implements OnInit, AfterViewInit {
-
-    @ViewChild('wrapperNode')
-    public wrapperNode: ElementRef;
+export class RecaptchaComponent implements OnInit {
 
     @Input('siteKey')
     public siteKey: string;
-
 
     public approved = false;
 
@@ -24,10 +20,6 @@ export class RecaptchaComponent implements OnInit, AfterViewInit {
     ngOnInit() {
         this.registerCallback();
         this.registerExpiredCallback();
-    }
-
-    ngAfterViewInit() {
-        this.approved = (this.wrapperNode.nativeElement.height) ? false : true;
     }
 
     public isApproved(): boolean {
