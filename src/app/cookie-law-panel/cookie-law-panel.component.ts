fc11c772db2b637ed06ebb9d9b6b33c762d58538
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-09-02 17:42:14 +0200

    cookie law improvements, list and table improvements. Needs to move mocks

--- src/app/cookie-law-panel/cookie-law-panel.component.ts
+++ src/app/cookie-law-panel/cookie-law-panel.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+export const COOKIE_LAW_STORAGE_NAME = 'cookie_law_seen';
 @Component({
     selector: 'sce-cookie-law-panel',
     templateUrl: './cookie-law-panel.component.html'
 })
-export class CookieLawPanelComponent {
+export class CookieLawPanelComponent implements OnInit {
 
-    public visible = true;
+    public visible: boolean;
     public text = `Używamy plików cookies i local storage aby ułatwić Ci korzystanie z naszego serwisu oraz do celów statystycznych.
     Jeśli nie blokujesz tych plików, to zgadzasz się na ich użycie oraz zapisanie w pamięci urządzenia.
     Pamiętaj, że możesz samodzielnie zarządzać cookies, zmieniając ustawienia przeglądarki.`;
 
+    ngOnInit() {
+        this.setVisibility();
+    }
+
+    public setVisibility(): void {
+        if (localStorage.getItem('COOKIE_LAW_STORAGE_NAME')) {
+            this.visible = false;
+        } else {
+            this.visible = true;
+        }
+    }
+
     public hide(): void {
+        localStorage.setItem('COOKIE_LAW_STORAGE_NAME', '1');
         this.visible = false;
     }
 }
