79e9b29e47b1258ab69b6469ead5da899e2091f6
Marcin Kołodziejczak <kolodziejczak.mn@gmail.com>
2018-09-29 14:57:57 +0200

    Removed unnecessary blank line

--- src/app/activation/activation.constants.ts
+++ src/app/activation/activation.constants.ts
@@ -1,5 +1,4 @@
 export const PARAM_NAME = 'deleteCode';
-
 export const LOADING_TEXT = 'Twój kod jest przetwarzany. Prosimy o chwilę cierpliwości...';
 export const SUCCESS_TEXT = 'Przekierowuję do aplikacji...';
 export const ERROR_TEXT = 'Wystąpił błąd w trakcie analizy kodu aktywacji. Spróbuj ponownie lub skontaktuj się z administracją.';
